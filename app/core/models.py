2c6d0cbc4fa4deb653d990a5e155cff2048fdec7
teppichKay <iamcowa@gmail.com>
2019-08-10 15:56:13 +0200

    feature: create recipe

--- app/core/models.py
+++ app/core/models.py
@@ -61,9 +61,11 @@ class Ingredient(models.Model):
 
 class Recipe(models.Model):
     """Recipe object"""
-    title = models.CharField(max_length=255)
     user = models.ForeignKey(
-        settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    title = models.CharField(max_length=255)
     time_minutes = models.IntegerField()
     price = models.DecimalField(max_digits=5, decimal_places=2)
     link = models.CharField(max_length=255, blank=True)
